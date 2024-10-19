b49435800a40becac885894adf914e266bbceb62
Bonney <popova.sofy@gmail.com>
2024-10-19 15:28:44 +0300

    сем6 упр1

--- семинар 6/упражнение 1/упражнение 1/упражнение_1.py
+++ семинар 6/упражнение 1/упражнение 1/упражнение_1.py
@@ -11,7 +11,7 @@ def calculate(*args):
 
 
 root = Tk()
-root.title("Feet to Meters")
+root.title("Calculator")
 
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -34,9 +34,7 @@ ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
 
 for child in mainframe.winfo_children(): 
